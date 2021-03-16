fc77e72262b0c9cfb0810b9d8be3eabb0d012a62
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-03-16 12:04:14 -0500

    move CIO constants to blockio.h

--- ncopy/src/blockio.h
+++ ncopy/src/blockio.h
@@ -5,6 +5,9 @@
 #ifndef BLOCKIO_H
 #define BLOCKIO_H
 
+#define D_DEVICE_DATA 2
+#define D_DEVICE_DIRECTORY 3
+
 void open(unsigned char channel, unsigned char aux1, char* buf, unsigned short len);
 void close(unsigned char channel);
 void get(unsigned char channel, char* buf, unsigned short len);
